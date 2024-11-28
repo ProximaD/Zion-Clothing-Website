cd3b908743a764ba605f33877a2226523400a06f
Sandra Lavenda <111272273+ProximaD@users.noreply.github.com>
2024-11-28 13:27:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+/*========================================= SHOW MENU ============================================*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
+
+  if(toggle && nav){
+    toggle.addEventListener('click', () =>{
+      nav.classList.toggle('show')
+    })
+  }
+}
+
+showMenu('nav-toggle', 'nav-menu')
